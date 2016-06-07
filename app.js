c3f54abb60d220a3531d0c77f553e01842dc6dc8
Christian Kuster <christian@kusti.ch>
2016-06-07 07:00:27 +0200

    Migrated express 3 to 4

--- app.js
+++ app.js
@@ -1,67 +1,68 @@
-var express = require('express'),
-  http = require('http'),
-  path = require('path'),
-  passport = require("passport");
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const passport = require("passport");
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const errorhandler = require('errorhandler');
 
-var env = process.env.NODE_ENV || 'development',
-  config = require('./config/config')[env];
+var env = process.env.NODE_ENV || 'development';
+const config = require('./config/config')[env];
+
+console.log('Using configuration', config);
 
 require('./config/passport')(passport, config);
 
 
 var app = express();
 
-app.configure(function () {
-  app.set('port', config.app.port);
-  app.set('views', __dirname + '/app/views');
-  app.set('view engine', 'jade');
-  app.use(express.logger('dev'));
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.session(
-    {
-      secret: 'this shit hits'
-    }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', config.app.port);
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jade');
+app.use(morgan('combined'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session(
+  {
+    resave: true,
+    saveUninitialized: true,
+    secret: 'this shit hits'
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function () {
-  console.log ("Development mode.");
-  app.use(express.errorHandler());
-});
-app.configure ('production', function () {
-  console.log ("Production mode.");
-});
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
-  res.render('500', { error: err }); 
+  res.render('500', {error: err});
 });
-
-app.use(function(req, res, next){
+/*
+app.use(function (req, res, next) {
   res.status(404);
   if (req.accepts('html')) {
     res.render('404',
       {
-        url : req.url
+        url: req.url
       });
     return;
   }
   if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
+    res.send({error: 'Not found'});
     return;
   }
   res.type('txt').send('Not found');
 });
-
+*/
 
 require('./config/routes')(app, config, passport);
 
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+app.listen(app.get('port'), function () {
+  console.log("Express server listening on port " + app.get('port'));
 });
